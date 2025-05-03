72d7de00b1d0b090628cb2b895f7720abeba3dfb
Aivemoussa <Baptistee.moussa@gmail.com>
2025-05-03 09:04:03 +0200

    Update index.js
    ajoute d'un ping pour empêcher l'extionction du bot
    )

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const client = new Client({
   ]
 });
 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot is alive!'));
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Web server is running.');
+});
+
 const TOKEN = process.env.TOKEN;
 const SALON_ID = '1367896135506726982'; // ID du salon où réagir
 
